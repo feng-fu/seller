21592023c11f8aa2582fc944446f9d20b5ab85c5
duocai <fengfu2016@gmail.com>
2018-03-20 23:23:50 +0800

    update func list， add pub front-end logic

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # seller
 A simple Used trading platform with react and express.
 
-# 使用express和react构建的一个简单二手交易平台
+## 使用express和react构建的一个简单二手交易平台
 
 1. 
 add user aliyun config in server/config/oss-config.js
@@ -15,3 +15,18 @@ add user aliyun config in server/config/oss-config.js
   accessKeySecret: 'xxx',
   bucket: 'xxxx'
 }
+
+
+## 功能开发
+
+* [x] 登录注册
+* [x] 使用redis控制用户session
+* [x] 个人信息完善（头像等）
+* [x] 商品列表
+* [x] 商品展示
+* [x] 商品发布
+* [x] 用户信息修改
+* [ ] 商品信息修改
+* [ ] 上下架
+* [ ] 买家卖家沟通
+* [ ] 下单
--- src/container/dashborad/manager/manager.js
+++ src/container/dashborad/manager/manager.js
@@ -1,7 +1,33 @@
 import React from 'react'
-import { List, TextareaItem, InputItem, Accordion } from 'antd-mobile';
+import { List, TextareaItem, InputItem, Accordion } from 'antd-mobile'
+import axios from 'axios'
 
 export default class Manager extends React.Component {
+  constructor() {
+    super()
+    this.state ={
+      content: '',
+      title: '',
+      price: ''
+    }
+    this.publishGoodsHandle = this.publishGoodsHandle.bind(this)
+    this.changeStateHandle = this.changeStateHandle.bind(this)
+  }
+  changeStateHandle(key, val) {
+    this.setState({
+      [key]: val
+    })
+  }
+  publishGoodsHandle() {
+    
+    axios.post(`/goods/add`, this.state)
+      .then(r => {
+        console.log(r)
+      })
+      .catch(e => {
+        console.log(e)
+      })
+  }
   render() {
     return (
       <Accordion defaultActiveKey="0" className="my-accordion" onChange={this.onChange}>
@@ -9,7 +35,8 @@ export default class Manager extends React.Component {
           <List>
             <InputItem
               placeholder="请输入标题"
-              ref={el => this.autoFocusInst = el}
+              value={this.state.title}
+              onChange={val => this.changeStateHandle('title', val)}
             >
             标题
             </InputItem>
@@ -17,15 +44,23 @@ export default class Manager extends React.Component {
               title="内容"
               placeholder="请输入内容"
               data-seed="logId"
+              value={this.state.content}
+              onChange={val => this.changeStateHandle('content', val)}
               autoHeight
-              ref={el => this.customFocusInst = el}
               rows={5}
               count={140}
             />
             <List.Item>
+
+            <InputItem
+              placeholder="请输入价格"
+              value={this.state.price}
+              onChange={val => this.changeStateHandle('price', val)}
+          >价格</InputItem>
+
               <div
                 style={{ width: '100%', color: '#108ee9', textAlign: 'center' }}
-                onClick={() => this.customFocusInst.focus()}
+                onClick={this.publishGoodsHandle.bind(this)}
               >
                 提交
               </div>
